f2f23eaa2202519694ec4b78e938963c780ca7dc
Mariah Tree <mariah.tree@gmail.com>
2016-06-04 10:49:20 -0500

    moved old files to archive and added necessary files for heroku node

--- spec/ExampleSpec.js
+++
@@ -1,7 +0,0 @@
-// require files here!
-describe("Example Spec", function(){
-  it("Does some functionality", function(){
-    // Testing code for this "it" block
-    // for example: expect(someVar).toEqual(true)
-  });
-});
