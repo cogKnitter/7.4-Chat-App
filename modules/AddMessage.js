bf2beaad6de743b8909d992e4d8b260eb9947457
Mariah Tree <mariah.tree@gmail.com>
2016-06-05 16:18:10 -0500

    fixed input so that it points to tiny tiny

--- modules/AddMessage.js
+++ modules/AddMessage.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import $ from 'jquery'
+import Serialize from 'form-serialize'
 
 export default React.createClass({
+  getInitialState(){
+    return {
+      messages: []
+    }
+  },
+  getDefaultProps(){
+    return {
+      source:"http://tiny-tiny.herokuapp.com/collections/mariah_chat"
+    }
+  },
+  handleSubmitMessage(e){
+    e.preventDefault();
+    var serializedForm = Serialize(this.refs.chatForm, {hash: true})
+    $.post(this.props.source, serializedForm, (resp)=> {
+      this.props.getMessages(),
+      this.refs.input.value= "";
+    })
+  },
   render () {
     return (
       <section>
-        <form className="message__form" method="POST" ref="chatForm" action="#" onSubmit={this.props.handleSubmitMessage}>
+        <form className="message__form" method="POST" ref="chatForm" action="#" onSubmit={this.handleSubmitMessage}>
           <input className="message__entry" type="text" name="chat" ref="input" placeholder="type your message here"/>
         </form>
       </section>
